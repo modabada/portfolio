a24e77442b8a28804b30bddbf394c5ea15dffa14
modabada <naninoo1103@gmail.com>
2025-06-24 23:34:18 +0900

    오타 수정

--- lib/Bloc/Theme/theme_bloc.dart
+++ lib/Bloc/Theme/theme_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfolio/Bloc/Theme/theme_event.dart';
 import 'package:portfolio/Bloc/Theme/theme_state.dart';
@@ -7,17 +5,21 @@ import 'package:portfolio/Core/Theme/app_theme.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
   ThemeBloc() : super(ThemeState.init()) {
-    on<ThemeEvent>((ThemeEvent event, emit) {
     on<ThemeEvent>((final ThemeEvent event, final Emitter<ThemeState> emit) {
-      if (event is ToggleThemeBtnPressed) {
-        emit(
-          state.fromAppTheme(
-            theme:
-                state.currentTheme is LightTheme ? DarkTheme() : LightTheme(),
-          ),
-        );
-      }
-      return;
+      on<ThemeEvent>(
+        (final ThemeEvent event, final Emitter<ThemeState> emit) {
+          if (event is ToggleThemeBtnPressed) {
+            emit(
+              state.fromAppTheme(
+                theme: state.currentTheme is LightTheme
+                    ? DarkTheme()
+                    : LightTheme(),
+              ),
+            );
+          }
+          return;
+        },
+      );
     });
   }
 }
