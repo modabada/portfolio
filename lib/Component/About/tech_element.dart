72bf63ed174683e909ef97c594f7c7b29d93781c
modabada <naninoo1103@gmail.com>
2025-07-19 23:35:32 +0900

    양식에 맞게 수정

--- lib/Component/About/tech_element.dart
+++ lib/Component/About/tech_element.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/gestures/events.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 @immutable
